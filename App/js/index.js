ed74d102ecf57c0d0ba983b18aac3b250107c81b
Shriramzz <88591981+Shriramzz@users.noreply.github.com>
2021-12-19 10:01:23 +0530

    Updated by CrossUI No-Code App Builder

--- App/js/index.js
+++ App/js/index.js
@@ -19,7 +19,27 @@ xui.Class('App', 'xui.Module',{
         iniComponents : function(){
             // [[Code created by CrossUI RAD Studio
             var host=this, children=[], append=function(child){children.push(child.get(0));};
-
+            
+            append(
+                xui.create("xui.UI.Button")
+                .setHost(host,"xui_ui_button10")
+                .setLeft("26.666666666666668em")
+                .setTop("20.571428571428573em")
+                .setWidth("7.542857142857143em")
+                .setHeight("5.866666666666666em")
+                .setCaption("ಬಿಳಿ ೨ ")
+            );
+            
+            append(
+                xui.create("xui.UI.Audio")
+                .setHost(host,"xui_ui_audio5")
+                .setLeft("12.952380952380953em")
+                .setTop("4.571428571428571em")
+                .setWidth("39.31428571428572em")
+                .setHeight("11.885714285714286em")
+                .setSrc("https://drive.google.com/file/d/1_rMGD8AQgDnI-Gkps1F_AmJlWN_tRBaK/view?usp=sharing")
+            );
+            
             return children;
             // ]]Code created by CrossUI RAD Studio
         },
